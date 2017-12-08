7d2a727c23f2251dc309e929194399ae1b5a9600
mrjln <marjolein_tromp@hotmail.com>
2017-12-08 17:43:27 +0100

    mock pokemon

---
+++ src/app/mock-pokemons.ts
@@ -0,0 +1,14 @@
+import { Pokemon } from './pokemon';
+
+export const POKEMONS: Pokemon[] = [
+  { id: 11, name: 'Squirtle' },
+  { id: 12, name: 'Charmander' },
+  { id: 13, name: 'Blastoise' },
+  { id: 14, name: 'Bulbasaur' },
+  { id: 15, name: 'Ivysaur' },
+  { id: 16, name: 'Venussaur' },
+  { id: 17, name: 'Wartortle' },
+  { id: 18, name: 'Charmeleon' },
+  { id: 19, name: 'Charizard' },
+  { id: 20, name: 'Raichu' }
+];
