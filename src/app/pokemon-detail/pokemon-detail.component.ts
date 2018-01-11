6ac342c37beb31d1b4a2dbd7780acf2d2050099a
mrjln <marjolein_tromp@hotmail.com>
2018-01-11 16:46:26 +0100

    one should place the @input inside the class

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from '../pokemon'
+
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.css']
 })
-export class PokemonDetailComponent implements OnInit {
 
+
+
+export class PokemonDetailComponent implements OnInit {
+@Input() pokemon: Pokemon;
   constructor() { }
 
   ngOnInit() {
